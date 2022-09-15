8e0a1ab2f618ff841e936097b3b7524fff677d84
Albert Guo <albertguo@alberts-mbp-2.myfiosgateway.com>
2022-09-15 13:29:27 -0400

    Added a CTA for search

--- shakesearch-ui/src/App.js
+++ shakesearch-ui/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <h1 className="welcome-text">Welcome to Shakesearch</h1>
         <h4 className="welcome-subtext">wherefore art thou exact matches?</h4>
+        <h4>Type a word into the search bar and be rewarded with lines from the Bard!</h4>
       </header>
       <Search />
     </div>
